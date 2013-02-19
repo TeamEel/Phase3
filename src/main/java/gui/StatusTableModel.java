04dc40892210665e397984331f1e3475c66fed39
James Thorne <james@thorne.bz>
2013-02-19 12:00:47 +0000

    added table to display status output

---
+++ src/main/java/gui/StatusTableModel.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+
+    
+/**
+ *
+ * @author James
+ */
+class StatusTableModel extends AbstractTableModel implements TableModel {
+    private final int size =7;
+    public StatusRow[] rows = new StatusRow[7];
+    public StatusTableModel() {
+    } 
+
+
+    @Override
+    public int getColumnCount(){return 2;}
+    @Override
+    public int getRowCount() { return size; }
+    @Override
+    public Object getValueAt(int row, int col) {
+        if(col==0) {return rows[row].Key();}
+        if(col==1) {return rows[row].Value();}
+        return null;
+    }
+
+
+    
+    
+}
